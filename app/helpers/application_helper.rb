8cb5aba4eab49ae5bb924e2eb8c739794452bf21
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-25 16:57:14 +0000

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
      end
     end
+    
+    def copyright_generator
+      GruckViewTool::Renderer.copyright 'Giancarlos Ruck', 'All rights reserved'
+    end
 end
